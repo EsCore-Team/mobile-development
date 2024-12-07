adeb3679d81789b3b9e592a83d3932af26ca10cc
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-12-07 11:57:56 +0700

    add color black -night mode on

--- app/src/main/java/com/dicoding/escore/view/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/escore/view/main/MainActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.TextView
